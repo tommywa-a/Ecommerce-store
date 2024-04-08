dd8abdd48d4d5c22852881917c26ca0e9032c308
tommywa-a <tomiwa95@gmail.com>
2024-04-08 21:26:48 +0100

    Environment setup and featured products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,27 @@
-const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+import getBillboard from '@/actions/get-billboard'
+import getProducts from '@/actions/get-products'
+import Billboard from '@/components/billboard'
+import ProductList from '@/components/product-list'
+import Container from '@/components/ui/container'
+
+export const revalidate = 0
+
+const HomePage = async () => {
+	const products = await getProducts({ isFeatured: true })
+	const billboard = await getBillboard('e45d2bf8-104e-4872-b445-e2930db376e0')
+	return (
+		<Container>
+			<div className='space-y-10 pb-10'>
+				<Billboard data={billboard} />
+				<div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+					<ProductList
+						title='Featured Products'
+						items={products}
+					/>
+				</div>
+			</div>
+		</Container>
+	)
 }
 
 export default HomePage
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export const images = {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'res.cloudinary.com',
+      port: '',
+      pathname: '/**',
+    },
+  ],
+};
