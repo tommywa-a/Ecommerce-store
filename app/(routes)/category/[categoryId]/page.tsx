e2547beafc95fb803303132f22635e870c5817f6
tommywa-a <tomiwa95@gmail.com>
2024-04-12 14:33:19 +0100

    Finished Mobile Filters

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -7,6 +7,7 @@ import Container from '@/components/ui/container'
 import Filter from './components/filter'
 import NoResults from '@/components/ui/no-results'
 import ProductCard from '@/components/ui/product-card'
+import MobileFilters from './components/mobile-filters'
 
 interface CategoryPageProps {
 	params: {
@@ -38,7 +39,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
 				<Billboard data={category.billboard} />
 				<div className='px-4 sm:px-6 lg:px-8 pb-24'>
 					<div className='lg:grid lg:grid-cols-5 lg:gap-x-8'>
-						{/* Add Mobile Filters */}
+						<MobileFilters sizes={sizes} colors={colors} />
 						<div className='hidden lg:block'>
 							<Filter
 								valueKey='sizeId'
