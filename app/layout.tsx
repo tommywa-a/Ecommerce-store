2384e94c1705c8764c222bdf18e9629f8d48303c
tommywa-a <tomiwa95@gmail.com>
2024-04-13 23:14:03 +0100

    Created Modal and Preview Modal on expand button

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modal-provider";
 
 export const revalidate = 0
 
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -2,19 +2,30 @@
 
 import { Product } from '@/types'
 import Image from 'next/image'
-import IconButton from '@/components/ui/icon-button'
-import { Expand, ShoppingCart } from 'lucide-react'
-import Currency from '@/components/ui/currency'
+import { MouseEventHandler } from 'react'
 import { useRouter } from 'next/navigation'
+import { Expand, ShoppingCart } from 'lucide-react'
+
+import IconButton from '@/components/ui/icon-button'
+import Currency from '@/components/ui/currency'
+import usePreviewModal from '@/hooks/use-preview-modal'
 
 interface ProductCardProps {
 	data: Product
 }
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
-  const router = useRouter()
-  const handleClick = () => {
+  const previewModal = usePreviewModal()
+	const router = useRouter()
+  
+	const handleClick = () => {
     router.push(`/product/${data?.id}`)
   }
+
+	const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+		event.stopPropagation()
+
+		previewModal.onOpen(data)
+	}
 
 	return (
 		<div onClick={handleClick} className='bg-white group cursor-pointer roundded-xl border p-3 space-y-4'>
@@ -29,7 +40,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
 				<div className='opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5'>
 					<div className='flex gap-x-6 justify-center'>
 						<IconButton
-							onClick={() => {}}
+							onClick={onPreview}
 							icon={
 								<Expand
 									size={20}
