d1aabe26edf61463b94b7fe2bd11b1b234f3a34b
tommywa-a <tomiwa95@gmail.com>
2024-04-14 23:41:36 +0100

    Formatted modal documents

--- components/preview-modal.tsx
+++ components/preview-modal.tsx
@@ -6,25 +6,28 @@ import Gallery from '@/components/gallery'
 import Info from '@/components/info'
 
 const PreviewModal = () => {
-  const previewModal = usePreviewModal()
-  const product = usePreviewModal((state) => state.data)
+	const previewModal = usePreviewModal()
+	const product = usePreviewModal((state) => state.data)
 
-  if (!product) {
-    return null
-  }
+	if (!product) {
+		return null
+	}
 
-  return (
-    <Modal open={previewModal.isOpen} onClose={previewModal.onClose}>
-      <div className='grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8'>
-        <div className="sm:col-span-4 lg:col-span-5">
-          <Gallery images={product.images} />
-        </div>
-        <div className='sm:col-span-8 lg:col-spa7'>
-          <Info data={product} />
-        </div>
-      </div>
-    </Modal>
-  )
+	return (
+		<Modal
+			open={previewModal.isOpen}
+			onClose={previewModal.onClose}
+		>
+			<div className='grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8'>
+				<div className='sm:col-span-4 lg:col-span-5'>
+					<Gallery images={product.images} />
+				</div>
+				<div className='sm:col-span-8 lg:col-spa7'>
+					<Info data={product} />
+				</div>
+			</div>
+		</Modal>
+	)
 }
 
 export default PreviewModal
--- hooks/use-preview-modal.ts
+++ hooks/use-preview-modal.ts
@@ -1,19 +1,19 @@
-import {create} from 'zustand'
+import { create } from 'zustand'
 
-import {Product} from '@/types'
+import { Product } from '@/types'
 
 interface PreviewModalStore {
-  isOpen: boolean
-  data?: Product
-  onOpen: (data: Product) => void
-  onClose: () => void
+	isOpen: boolean
+	data?: Product
+	onOpen: (data: Product) => void
+	onClose: () => void
 }
 
 const usePreviewModal = create<PreviewModalStore>((set) => ({
-  isOpen: false,
-  data: undefined,
-  onOpen: (data: Product) => set({data, isOpen: true}),
-  onClose: () => set({isOpen: false, data: undefined}),
+	isOpen: false,
+	data: undefined,
+	onOpen: (data: Product) => set({ data, isOpen: true }),
+	onClose: () => set({ isOpen: false, data: undefined }),
 }))
 
 export default usePreviewModal
