95a1df9a582bb15b3e7ab3e1a7e7dab0ba3ac007
tommywa-a <tomiwa95@gmail.com>
2024-04-18 13:13:40 +0100

    Added functionality to remove item from cart

--- app/(routes)/cart/components/cart-item.tsx
+++ app/(routes)/cart/components/cart-item.tsx
@@ -16,6 +16,12 @@ interface CartItemProps {
 const CartItem: React.FC<CartItemProps> = ({
   data
 }) => {
+  const cart = useCart()
+
+  const onRemove = () => {
+    cart.removeItem(data.id)
+  }
+
   return (
     <li className='flex py-6 border-b'>
       <div className='relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48'>
@@ -28,7 +34,7 @@ const CartItem: React.FC<CartItemProps> = ({
       </div>
       <div className='relative ml-4 flex-1 flex flex-col justify-between sm:ml-6'>
         <div className="absolute z-10 right-0 top-0">
-          <IconButton onClick={() => {}} icon={<X size={15} />} />
+          <IconButton onClick={onRemove} icon={<X size={15} />} />
         </div>
         <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
           <div className="flex justify-between">
