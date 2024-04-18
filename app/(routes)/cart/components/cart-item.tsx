1db3a95fe00380057360bd614b1e1411bca553aa
tommywa-a <tomiwa95@gmail.com>
2024-04-18 13:09:56 +0100

    Cart item

--- app/(routes)/cart/components/cart-item.tsx
+++ app/(routes)/cart/components/cart-item.tsx
@@ -26,7 +26,24 @@ const CartItem: React.FC<CartItemProps> = ({
           className='object-cover object-center'
         />
       </div>
-      div
+      <div className='relative ml-4 flex-1 flex flex-col justify-between sm:ml-6'>
+        <div className="absolute z-10 right-0 top-0">
+          <IconButton onClick={() => {}} icon={<X size={15} />} />
+        </div>
+        <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
+          <div className="flex justify-between">
+            <p className="text-lg font-semibold text-black">
+              {data.name}
+            </p>
+          </div>
+
+          <div className='mt-1 flex text-sm'>
+            <p className="text-gray-500">{data.color?.name}</p>
+            <p className="text-gray-500 ml-4 border-l border-gray-200 pl-4">{data.size?.name}</p>
+          </div>
+          <Currency value={data.price} />
+        </div>
+      </div>
     </li>
   )
 }
