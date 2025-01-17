db2c2c55485b6778ebd11bef6ef4ae70db4cce74
Tomiwa Aderibigbe <tomiwa95@gmail.com>
2025-01-17 15:44:59 +0100

    Changed grid size for small devices in categories page

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -54,7 +54,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
 						</div>
 						<div className='mt-6 lg:col-span-4 lg:mt-0'>
 							{products.length === 0 && <NoResults />}
-							<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+							<div className='grid grid-cols-2 md:grid-cols-3 gap-4'>
 								{products.map((item) => (
 									<ProductCard
 										key={item.id}
