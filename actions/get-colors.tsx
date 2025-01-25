05042b16e8e8cd4ca7b2390c93707566bdcf42bd
Tomiwa Aderibigbe <tomiwa95@gmail.com>
2025-01-25 10:59:57 +0100

    Modified get-colors action to handle errors as well

--- actions/get-colors.tsx
+++ actions/get-colors.tsx
@@ -1,11 +1,20 @@
-import {Color} from "@/types"
+import {Color} from "@/types";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`;
 
 const getColors = async (): Promise<Color[]> => {
-  const res = await fetch(URL, { cache: 'no-cache' })
-  
-  return res.json()
-}
+  try {
+    const res = await fetch(URL, { cache: 'no-cache' });
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch colors: ${res.status} ${res.statusText}`);
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
 export default getColors
