6d7e108a9f521688196b580717559b42cbd597ba
Tomiwa Aderibigbe <tomiwa95@gmail.com>
2025-01-26 10:13:06 +0100

    Changed footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className='bg-white border-t'>
       <div className='mx-auto py-10'>
         <p className='text-center text-xs text-black'>
-          &copy; {new Date().getFullYear()} Ecommerce Store. All rights reserved.
+          &copy; {new Date().getFullYear()} Ecommerce Store by Tomiwa. All rights reserved.
         </p>
       </div>
     </footer>
